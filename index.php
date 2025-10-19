b294629dc582f9a4f11c45d489a40d3c6b83427c
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2025-10-20 02:12:44 +0330

    Fix layout by allowing Bootstrap assets to load

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ $structuredData = buildStructuredData($business);
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet" integrity="sha384-iC0mJCPTCVBn2YFKro7B1vfnv9lkrdTuaNdcvE9ZF8vD6ofXIjdlY6jFy/aAEPde" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.1/css/all.min.css" rel="stylesheet">
     <style>
         :root {
@@ -683,7 +683,7 @@ $structuredData = buildStructuredData($business);
     </div>
 </footer>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-s1e+zLz6O7cHK5xrh8gQicxXnc9cXgcav+qEq/n2m8jiPpdSUfEpjaFpZ5Yl8ys9" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
 <?php
